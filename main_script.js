5651d11e4e978aa290d8a242a74e3c74fd72081d
aks_shar008 <akshay.w3web@gmail.com>
2025-01-08 20:37:20 +0530

    newCodeUpdated

--- main_script.js
+++ main_script.js
@@ -2,66 +2,46 @@ $.ajax({
   url: "./data.json",
   type: "GET",
   success: function (data) {
-    // console.log(myData);
     showData(data);
-    // box(myData, 0);
   },
 });
 
 function showData(data) {
   let mainBox = `<div class="inner1">
     <h3>${data[0].product}</h3>
-    <img class="img" src="${data[0].details.image}" alt="" />
+    <img class="img" src="${data[0].image}" alt="" />
     </div>
   <div class="inner2">
     <p class="light">HTS Code</p>
-    <p>${data[0].details.HTS_Code}</p>
+    <p>${data[0].HTS_Code}</p>
 
     <p class="light">Purchase Orders</p>
-    <p>${data[0].details.purchaseOrder}</p>
+    <p>${data[0].purchaseOrder}</p>
 
     <div class="twoItems">
       <div class="innerTI">
         <p class="light">Quantity</p>
-        <p>${data[0].details.quantity}</p>
+        <p>${data[0].quantity}</p>
       </div>
       <div class="innerTI">
         <p class="light">Main Parts</p>
-        <p>${data[0].details.MainParts}</p>
+        <p>${data[0].MainParts}</p>
       </div>
     </div>
   </div>`;
   $(".containerElement").append(mainBox);
 
-  let type = "";
-  let l1 = data[0].details.children[0].child1Level.length;
-  let l2 = data[0].details.children[0].child1Level[0].child2Level.length;
-  let l3 = data[0].details.children[0].child1Level[0].child2Level[0].child3Level.length;
-  let l4 = data[0].details.children[0].child1Level[0].child2Level[0].child3Level[0].child4Level[0].child5Level[0].child6Level.length;
-  //   let l3 = data[0].details.children[0].child1Level[0].child2Level[0].length;
-  //   console.log(max_children.length);
-  //   for (let i = 0; i < max_children.length; i++) {
-  // if (i == 0) {
-  //   type = "l1";
-  //   break;
-  // } else if (i == 1) {
-  //   type = "l2";
+  let l1 = data.length;
+  let l2 = data[0].childLevel1.child_l1Box1[0].child_l2Boxes.length;
+  let l3 =
+    data[0].childLevel1.child_l1Box1[0].child_l2Boxes[0].child_l3Boxes.length;
+  // let l4 = data[0].details.children[0].child1Level[0].child2Level[0].child3Level[0].child4Level[0].child5Level[0].child6Level.length;
 
-  //   break;
-  // } else if (i == 2) {
-  //   type = "l3";
-  // } else if (i == 3) {
-  //   type = "l4";
-  // } else {
-  //   type = "l5";
-  // }
-  // console.log(l2);
   boxDataRec(data, l1, "l1");
   boxDataRec(data, l2, "l2");
   boxDataRec(data, l3, "l3");
-  boxDataRec(data, l4, "l4");
-  // console.log(l4);
-  //   }
+  // boxDataRec(data, l4, "l4");
+  // console.log(l3);
 }
 
 function boxDataRec(data, iterator, type) {
@@ -69,9 +49,7 @@ function boxDataRec(data, iterator, type) {
   if (type == "l1") {
     // console.log(iterator);
     for (let i = 0; i < iterator; i++) {
-      //  console.log(data[0].details.children[i].child1Level[i].title);
-      let dataAccessor = data[0].details.children[i].child1Level[i];
-
+      let dataAccessor = data[0].childLevel1.child_l1Box1[i];
       let l1Box = `<div class="subBox" style="display: inline-block;">
                 <div class="bluebox">
                     <h3>${dataAccessor.title}</h3>
@@ -106,12 +84,16 @@ function boxDataRec(data, iterator, type) {
       $(".child").append(l1Box);
     }
   }
-  //   Level 2
+  //   Level 2 -- Pending 
   if (type == "l2") {
     for (let i = 0; i < iterator; i++) {
-      let dataAccessor =
-        data[0].details.children[0].child1Level[0].child2Level[i];
+      let dataAccessor = data[0].childLevel1.child_l1Box1[0].child_l2Boxes[i];
       // console.log(dataAccessor);
+
+      // Leaving Toggle Data on Scroll 
+      // if(){
+
+      // }
       let l2Box = `<div class="subBox" style="display: inline-block;">
     <div class="bluebox">
         <h3>${dataAccessor.title}</h3>
@@ -153,15 +135,12 @@ function boxDataRec(data, iterator, type) {
   }
   //   Level 3
   if (type == "l3") {
-    // console.log(iterator);
-    for (let i = 0; i <= iterator; i++) {
-      //  console.log(data[0].details.children[i].child1Level[i].title);
-      let dataAccessor =
-        data[0].details.children[0].child1Level[0].child2Level[0].child3Level[0]
-          .child4Level[i];
-      //   console.log(dataAccessor.title);
-      if (dataAccessor.company1name) {
-        let l3Box = `<div class="subBox" style="display: inline-block;">
+    for (let i = 0; i < iterator; i++) {
+      let dataAccessor = data[0].childLevel1.child_l1Box1[0].child_l2Boxes[0].child_l3Boxes[i];
+      let parentIterator = data[0].childLevel1.child_l1Box1[0].child_l2Boxes.length;
+      // console.log(parentIterator);
+      // console.log(dataAccessor.title);
+      let l3Box = `<div class="subBox" style="display: inline-block;">
                 <div class="bluebox">
                     <h3>${dataAccessor.title}</h3>
                     <div class="bundle">
@@ -192,42 +171,20 @@ function boxDataRec(data, iterator, type) {
                     </div>
                     </div>
                 </div>`;
-        $(`.cb${i}`).append(`<div class="subContainer">
-                    <div class="lev${i}">${l3Box}</div>
-                </div>
-            `);
-      } else {
-        let l3Box = `<div class="subBox" style="display: inline-block;">
-                <div class="bluebox">
-                    <h3>${dataAccessor.title}</h3>
-                    <div class="bundle">
-                        <img class="icon" src="${dataAccessor.shieldImage}" alt="">
-                        <p>${dataAccessor.shieldValue}</p>
-                    </div>
-                </div>
-                <div class="whiteBox">
-                    <div class="flex">
-                        <div>${dataAccessor.value}</div>
-                        <div class="right">
-                            <img class="icon" src="${dataAccessor.editLogo}" alt="">
-                            <p>Add</p>
-                        </div>
-                    </div>
-                    </div>
-                </div>`;
-        $(`.cb${i}`).append(`<div class="subContainer">
-                    <div class="lev${i}">${l3Box}</div>
-                </div>
-            `);
-      }
-    }
-  }
-  if (type == "l4") {
-    for (let i = 0; i <= iterator; i++) {
-      //  console.log(data[0].details.children[i].child1Level[i].title);
-      let dataAccessor = data[0].details.children[0].child1Level[0].child2Level[0].child3Level[0].child4Level[0].child5Level[0].child6Level[i];
-      // console.log(dataAccessor);
-      
+                for(let j=0; j<parentIterator; j++){
+                  $(`.cb${[j]}`).append(
+                    `<div class="subContainer">
+                      <div class="lev${2*(i+1)-2}">${l3Box}</div>
+                      <div class="lev${2*(i+1)}">${l3Box}</div>
+                    </div>`
+                  );
+                }
+                //   $(`.cb${i}`).append(`<div class="subContainer">
+                //   <div class="lev${2*(i+1)-2}">${l3Box}</div>
+                //   <div class="lev${2*(i+1)}">${l3Box}</div>
+                //  </div>
+                // `);
+
     }
   }
 }
